0001f28deeeac06640ea4cf6b0a01ac4b8e783be
FcoMdz <fcodmendez24@gmail.com>
2024-05-08 22:21:59 -0600

    se cambió la versión de mysql a mysql2

--- Back/configuraciones/connection.js
+++ Back/configuraciones/connection.js
@@ -1,7 +1,7 @@
 // Importa los paquetes necesarios
 require('dotenv').config();
 const express = require('express');
-const mysql = require('mysql');
+const mysql = require('mysql2');
 const app = express();
 const cors = require('cors');
 // Habilita CORS
